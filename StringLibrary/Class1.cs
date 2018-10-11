49216a07a6f1a2319d7a6291b8e7f92f056ce4e5
VeronikaMakukho <v.makuho@gmail.com>
2018-10-11 22:43:33 +0300

    Добавьте файлы проекта.

---
+++ StringLibrary/Class1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace StringLibrary
+{
+
+    public static class StringLibrary
+    {
+        
+        #region EuclideanAlgorithm
+
+        public static int EuclidAlgorithm(int firstNumber, int secondNumber)
+        {
+            int temp;
+            while (secondNumber != 0)
+            {
+                temp = secondNumber;
+                secondNumber = firstNumber % secondNumber;
+                firstNumber = temp;
+            }
+            return Math.Abs(firstNumber);
+        }
+
+
+         
+
+
+        #region BinaryEuclideanAlgorithm
+
+        public static int BinaryEuclidAlgorithm(int firstNumber, int secondNumber)
+        {
+            if (firstNumber == 0)
+            {
+                return secondNumber;
+            }
+            if (secondNumber == 0)
+            {
+                return firstNumber;
+            }
+            if (firstNumber == secondNumber)
+            {
+                return firstNumber;
+            }
+            if (firstNumber == 1 || secondNumber == 1)
+            {
+                return 1;
+            }
+            if ((firstNumber % 2 == 0) && (secondNumber % 2 == 0))
+            {
+                return 2 * BinaryEuclidAlgorithm(firstNumber >> 1, secondNumber >> 1);
+            }
+            if ((firstNumber % 2 == 0) && (secondNumber % 2 != 0))
+            {
+                return BinaryEuclidAlgorithm(firstNumber >> 1, secondNumber);
+            }
+            if ((firstNumber % 2 != 0) && (secondNumber % 2 == 0))
+            {
+                return BinaryEuclidAlgorithm(firstNumber, secondNumber >> 1);
+            }
+            if (firstNumber % 2 != 0 && secondNumber % 2 != 0 && firstNumber > secondNumber)
+            {
+                return BinaryEuclidAlgorithm((firstNumber - secondNumber) >> 1, secondNumber);
+            }
+            if (firstNumber % 2 != 0 && secondNumber % 2 != 0 && firstNumber < secondNumber)
+            {
+                return BinaryEuclidAlgorithm((secondNumber - firstNumber) >> 1, firstNumber);
+            }
+            return 0;
+        }
+
+
+
+
+
+
+        #endregion
+
+        #region Newton method        
+
+        public static double Radical(double number, int power, double accuracy)
+        {
+            if (number < 0 && power % 2 == 0 || power == 0) throw new ArgumentException();
+            if (number == 0) return 0;
+            double x = 0, result = 1;
+            do
+            {
+                x = result;
+                result = ((power - 1) * x + number / Math.Pow(x, power - 1)) / power;
+
+            } while (Math.Abs(x - result) > accuracy);
+            return result;
+        }
+        #endregion
+
+
+
+
+    } }
+#endregion}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using 
+using System.Diagnostics;
+
+namespace MathMethodsTests
+{
+    [TestClass]
+    public class StringlibraryTests
+    {
+        
+        #region EuclidAlgorithmTest
+        [TestMethod]
+        public void EuclidAlgorithmTestMethod()
+        {
+            int first = 789;
+            int second = 6;
+            
+            Assert.AreEqual(1, Stringlibrary.EuclidAlgorithm(first, second, ));
+        }
+
+       
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void ExeptionEuclidAlgorithmTestMethod()
+        {
+            int[] numbers = null;
+            Stringlibrary.EuclidAlgorithm(numbers);
+        }
+        #endregion
+
+        #region BinaryEuclidAlgorithmTest
+        [TestMethod]
+        public void BinaryEuclidAlgorithmTestMethod1()
+        {
+            int first = 7435446;
+            int second = 7;
+           
+            Assert.AreEqual(1, Stringlibrary.BinaryEuclidAlgorithm(first, second));
+        }
+
+       
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void ExeptionBinaryEuclidAlgorithmTestMethod()
+        {
+            int[] numbers = null;
+            Stringlibrary.BinaryEuclidAlgorithm(numbers);
+        }
+        #endregion
+
+        #region NewtonMethodTest
+        [TestMethod]
+        public void NewtonMethodTestMethod1()
+        {
+            double number = 156;
+            int power = 3;
+            double accuracy = 0;
+            Assert.AreEqual(8, StringlibraryTests.Radical(number, power, accuracy));
+        }
+
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ExeptionNewtonMethodTestMethod()
+        {
+            double number = -1030;
+            int power = 2;
+            double accurancy = 0;
+            Stringlibrary.Radical(number, power, accurancy);
+        }
+
+        
+        #endregion
+    }
+}
